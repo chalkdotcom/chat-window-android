a0b6fea329dffeab3ff72a17dd74e3ffc44397be
Daniel Rampelt <drampelt@gmail.com>
2020-08-14 14:50:26 -0400

    chore: migrate to AndroidX

--- inappchat/src/main/java/com/livechatinc/inappchat/ChatWindowActivity.java
+++ inappchat/src/main/java/com/livechatinc/inappchat/ChatWindowActivity.java
@@ -2,7 +2,7 @@ package com.livechatinc.inappchat;
 
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import androidx.fragment.app.FragmentActivity;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.FrameLayout;
